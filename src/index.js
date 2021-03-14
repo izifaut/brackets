ad5cde1403b4f31e1b666777243c57f8fc7d2c63
izifaut <79901531+izifaut@users.noreply.github.com>
2021-03-14 15:20:39 +0300

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let stack = [];
+  for (let letter of str) {
+    let bracketsIndex = bracketsConfig.indexOf(letter)
+    
+    if (bracketsIndex === -1){
+      continue
+    }
+
+    if(bracketsIndex % 2 === 0) {
+      stack.push(bracketsIndex + 1)
+    } else {
+      if(stack.pop() !== bracketsIndex) {
+        return false;
+      }
+    }
+  }
+  return stack.length === 0
 }
